de4ee9ced343d18629fb72e9cb72c94ac61a4dd2
LauraDRA <laura.delrio.aviles@gmail.com>
2016-10-24 05:19:01 +0200

    task structure

--- app/routes.js
+++ app/routes.js
@@ -13,6 +13,7 @@ module.exports = function(app) {
     // Insert routes below
     router.use('/example', require('./api/example/index'));
     router.use('/center', require('./api/center/index'));
+    router.use('/task', require('./api/task/index'));
 
     // Set a prefix for all calls
     app.use('/centermode/v1', router);
