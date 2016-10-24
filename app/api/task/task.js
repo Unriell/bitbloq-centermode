72edc6dbeb332630d28124ac536f6b782c0e34d5
LauraDRA <laura.delrio.aviles@gmail.com>
2016-10-24 05:26:29 +0200

    clean task

---
+++ app/api/task/task.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Create task
+ * @param req
+ * @param res
+ */
+exports.createTask = function(req, res) {
+
+};
+
+/**
+ * Get student task
+ * @param req
+ * @param res
+ */
+exports.getTask = function(req, res) {
+
+};
+
+/**
+ * Update a task if user is owner
+ * @param req
+ * @param res
+ */
+exports.updateTask = function(req, res) {
+
+};
+
+/**
+ * Delete a task if user is owner
+ * @param req
+ * @param res
+ */
+exports.deleteTask = function(req, res) {
+
+};
