b343461a865758d8f1dda2fe10214aea03fe24c4
LauraDRA <laura.delrio.aviles@gmail.com>
2017-01-03 11:16:35 +0100

    get an exercise by Id

--- app/api/exercise/exercise.controller.js
+++ app/api/exercise/exercise.controller.js
@@ -37,7 +37,24 @@ exports.create = function(req, res) {
  * @param res
  */
 exports.get = function(req, res) {
-
+    Exercise.findById(req.params.id)
+        .populate('creator', 'username')
+        .populate('teacher', 'username')
+        .exec(function(err, exercise) {
+            if (err) {
+                console.log(err);
+                err.code = parseInt(err.code) || 500;
+                res.status(err.code).send(err);
+            } else if (!exercise) {
+                res.sendStatus(404);
+            } else {
+                if (String(exercise.teacher._id) === String(req.user._id)) {
+                    res.status(200).json(exercise);
+                } else {
+                    res.sendStatus(401);
+                }
+            }
+        });
 };
 
 /**
